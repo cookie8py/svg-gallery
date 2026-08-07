4307e243d53a8d9982cb5f4b024a2d08bde12d4c
CookiePie <eunjongdang@gmail.com>
2026-08-07 11:38:12 +0900

    fixed

--- js/data/icons.js
+++ js/data/icons.js
@@ -1,10 +1,11 @@
 import { climateIcons } from "./icons/climate-icons.js";
 import { animalIcons } from "./icons/animal-icons.js";
 import { oceanIcons } from "./icons/ocean-icons.js";
-import { animatedIcons } from "./animated-icons.js";
+import { animatedIcons } from "./icons/animated-icons.js";
 
 export const icons = [
     ...climateIcons,
     ...animalIcons,
-    ...oceanIcons
+    ...oceanIcons,
+    ...animatedIcons
 ];
