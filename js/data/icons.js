55549ca218ef6b7fb25591919cd56312c5b121fa
CookiePie <eunjongdang@gmail.com>
2026-08-11 17:19:35 +0900

    pumpjack

--- js/data/icons.js
+++ js/data/icons.js
@@ -3,9 +3,26 @@ import { animalIcons } from "./icons/animal-icons.js";
 import { oceanIcons } from "./icons/ocean-icons.js";
 import { animatedIcons } from "./icons/animated-icons.js";
 
-export const icons = [
+const allIcons = [
     ...climateIcons,
     ...animalIcons,
     ...oceanIcons,
     ...animatedIcons
+];
+
+const completedIcons = allIcons.filter(
+    function (icon) {
+        return icon.previewType !== "placeholder";
+    }
+);
+
+const placeholderIcons = allIcons.filter(
+    function (icon) {
+        return icon.previewType === "placeholder";
+    }
+);
+
+export const icons = [
+    ...completedIcons,
+    ...placeholderIcons
 ];
