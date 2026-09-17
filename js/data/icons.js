7a78c686ec0be5b89461e261048a24d65b0ef856
CookiePie <eunjongdang@gmail.com>
2026-09-17 15:55:14 +0900

    we need to audit this

--- js/data/icons.js
+++ js/data/icons.js
@@ -1,42 +1,28 @@
-import {
-    climateIllustrations
-} from "./illustrations/climate-illustrations.js";
+import { climateIcons } from "./icons/climate-icons.js";
+import { animalIcons } from "./icons/animal-icons.js";
+import { oceanIcons } from "./icons/ocean-icons.js";
+import { animatedIcons } from "./icons/animated-icons.js";
 
-import {
-    animalIllustrations
-} from "./illustrations/animal-illustrations.js";
-
-import {
-    oceanIllustrations
-} from "./illustrations/ocean-illustrations.js";
-
-const allIllustrations = [
-    ...climateIllustrations,
-    ...animalIllustrations,
-    ...oceanIllustrations
+const allIcons = [
+    ...climateIcons,
+    ...animalIcons,
+    ...oceanIcons,
+    ...animatedIcons
 ];
 
-const completedIllustrations =
-    allIllustrations.filter(
-        function (illustration) {
-            return (
-                illustration.previewType !==
-                "placeholder"
-            );
-        }
-    );
+const completedIcons = allIcons.filter(
+    function (icon) {
+        return icon.previewType !== "placeholder";
+    }
+);
 
-const placeholderIllustrations =
-    allIllustrations.filter(
-        function (illustration) {
-            return (
-                illustration.previewType ===
-                "placeholder"
-            );
-        }
-    );
+const placeholderIcons = allIcons.filter(
+    function (icon) {
+        return icon.previewType === "placeholder";
+    }
+);
 
-export const illustrations = [
-    ...completedIllustrations,
-    ...placeholderIllustrations
+export const icons = [
+    ...completedIcons,
+    ...placeholderIcons
 ];
